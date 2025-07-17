be7fca5a94ca75d796d11f09b063b9cf828d958b
FerchoJpt-Controlc <jesustarax6@gmail.com>
2025-07-16 19:14:57 -0600

    Buscar por carnet

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 estudiantes={}
 
 def RegistrarEstudiante():
-    cantidad=int(input("ingrese cuantos estudiantes decea ingresar: "))
+    cantidad=int(input("\ningrese cuantos estudiantes decea ingresar: "))
 
     for i in range(cantidad):
-        carnet=input("ingrese carnet del estudiante: ")
-        nombre=input("ingrese nombre del estudiante: ")
-        edad=input("ingrese edad del estudiante: ")
-        carrera=input("ingrese carrera del estudiante: ")
+        carnet=input("\ningrese carnet del estudiante: ")
+        nombre=input("\ningrese nombre del estudiante: ")
+        edad=input("\ningrese edad del estudiante: ")
+        carrera=input("\ningrese carrera del estudiante: ")
 
         cursos={}
-        numCursos=int(input("ingrese cuantos cursos desea registrar: "))
+        numCursos=int(input("\ningrese cuantos cursos desea registrar: "))
         for i in range(numCursos):
-            nomCurso=input("ingrese la clave del curso: ")
-            notaTarea=float(input("ingrese nota tarea: "))
-            notaParcial=float(input("ingrese nota parcial: "))
-            notaProyecto=float(input("ingrese nota proyecto: "))
+            nomCurso=input("\ningrese la clave del curso: ")
+            notaTarea=float(input("\ningrese nota tarea: "))
+            notaParcial=float(input("\ningrese nota parcial: "))
+            notaProyecto=float(input("\ningrese nota proyecto: "))
 
             cursos[nomCurso]={
                 "notaTarea":notaTarea,
@@ -31,7 +31,19 @@ def RegistrarEstudiante():
             "cursos":cursos,
         }
 
-def MostrarEstudiantes():
+def BuscarEstudiante():
+    print("\nBúsqueda de estudiante")
+    buscado = input("Ingrese el número de carnet que desea buscar: ")
+
+    if buscado in estudiantes:
+        estudiante = estudiantes[buscado]
+        print("\nEstudiante encontrado:")
+        print(f"Nombre: {estudiante['nombre']}")
+        print(f"Edad: {estudiante['edad']}")
+        print(f"Carrera: {estudiante['carrera']}")
+
+    else:
+        print("\nEstudiante no encontrado.")
 
 
 def Menu():
@@ -41,11 +53,12 @@ def Menu():
         print("2. Mostrar estudiantes")
         print("3. Buscar por carnet")
         print("4. salir")
-        opcion=int(input("ingrese una opcion: "))
+        opcion=int(input("\ningrese una opcion: "))
         if opcion==1:
             RegistrarEstudiante()
        # elif opcion==2:
-       # elif opcion==3:
+        elif opcion==3:
+            BuscarEstudiante()
         elif opcion==4:
             print("gracias por usaar el sistemka ")
             break
